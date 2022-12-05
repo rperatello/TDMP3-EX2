71e89fda913ac4dc3ba500a05138aaa5457654df
Roger <roger.peratello@hotmail.com>
2022-12-04 23:05:02 -0300

    Projeto Inicial

---
+++ TDMP3-EX2/Lembrete.swift
@@ -0,0 +1,18 @@
+//
+//  Lembrete.swift
+//  TDMP3-EX2
+//
+//  Created by Roger Peratello on 04/12/22.
+//
+
+import Foundation
+
+class Lembrete {
+    var texto : String!
+    var concluido: Bool!
+    
+    init(texto: String, concluido: Bool){
+        self.texto = texto
+        self.concluido = concluido
+    }
+}
--- TDMP3-EX2/ViewController.swift
+++ TDMP3-EX2/ViewController.swift
@@ -7,11 +7,71 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
+    var lembretes : [Lembrete] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        /*
+        lembretes.append("Áries")
+        lembretes.append("Aquários")
+        lembretes.append("Gêmeos")
+        */
+        
+        var lembrete : Lembrete
+        
+        lembrete = Lembrete(texto: "Viagem SP", concluido: false)
+        lembretes.append(lembrete)
+        lembrete = Lembrete(texto: "Viagem Bahia", concluido: false)
+        lembretes.append(lembrete)
+        lembrete = Lembrete(texto: "Viagem Brotas", concluido: true)
+        lembretes.append(lembrete)
+        
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return lembretes.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let celulaReuso = "celulaReuso"
+        let lembrete : Lembrete = lembretes[indexPath.row]
+        
+        let celula = tableView.dequeueReusableCell(withIdentifier: celulaReuso, for: indexPath) as! LembreteCelula
+        
+        //celula.textLabel?.text = lembrete.texto
+        
+        celula.tituloLabel.text = lembrete.texto
+        var status : String
+        if (lembrete.concluido){
+            status = "concluído"
+        }
+        else{
+            status = "pendente"
+        }
+        celula.statusLabel?.text = status
+        
+        return celula
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?){
+        if segue.identifier == "detalheLembrete" {
+            if let indexPath = tableView.indexPathForSelectedRow {
+                let lembreteSelecionado = self.lembretes[indexPath.row]
+                
+                let viewControllerDestino = segue.destination as! DetalhesLembrete
+                
+                viewControllerDestino.lembrete = lembreteSelecionado
+            }
+        }        
     }
 
 
